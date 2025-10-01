d4a791a3f7feb028566f87f903f500bda6a6148e
GaborZeller <gabriel.d.celery@gmail.com>
2025-10-01 23:05:43 +0100

    feat: continue improving the code and add more Limit features

--- src/order_book.rs
+++ src/order_book.rs
@@ -1,21 +1,33 @@
 use time::OffsetDateTime;
+use uuid::Uuid;
 
 enum Order {
-    Bid { size: f64, timestamp: i64 },
-    Ask { size: f64, timestamp: i64 },
+    Bid { id: Uuid, size: f64, timestamp: i64 },
+    Ask { id: Uuid, size: f64, timestamp: i64 },
 }
 
 impl Order {
     fn new_bid(size: f64) -> Self {
-        let now = OffsetDateTime::now_utc();
-        let timestamp = now.unix_timestamp();
-        return Order::Bid { size, timestamp };
+        return Order::Bid {
+            id: Uuid::new_v4(),
+            size,
+            timestamp: OffsetDateTime::now_utc().unix_timestamp(),
+        };
     }
 
     fn new_ask(size: f64) -> Self {
-        let now = OffsetDateTime::now_utc();
-        let timestamp = now.unix_timestamp();
-        return Order::Ask { size, timestamp };
+        return Order::Ask {
+            id: Uuid::new_v4(),
+            size,
+            timestamp: OffsetDateTime::now_utc().unix_timestamp(),
+        };
+    }
+
+    fn id(&self) -> Uuid {
+        match self {
+            Order::Bid { id, .. } => *id,
+            Order::Ask { id, .. } => *id,
+        }
     }
 
     fn size(&self) -> f64 {
@@ -28,32 +40,44 @@ impl Order {
 
 // A group of orders at a certain price level
 // a bucket of orders that are of different sizes sitting on the same price level
-struct Limit {
+struct Limit<'a> {
     price: f64,
-    orders: Vec<Order>,
+    orders: Vec<&'a Order>,
     total_volume: f64,
 }
 
-impl Limit {
-    fn new(price: f64) -> Self {
-        return Self {
+impl<'a> Limit<'a> {
+    fn new(price: f64) -> Limit<'a> {
+        return Limit {
             price,
             orders: vec![],
             total_volume: 0.0,
         };
     }
 
-    fn add_order(&mut self, o: Order) {
+    fn add_order(&mut self, o: &'a Order) {
         self.total_volume += o.size();
         self.orders.push(o);
     }
+
+    fn remove_order(&mut self, order_id: Uuid) -> Result<(), String> {
+        let index = self.orders.iter().position(|x: &&Order| x.id() == order_id);
+        match index {
+            Some(i) => {
+                let removed_order = self.orders.swap_remove(i);
+                self.total_volume -= removed_order.size();
+                Ok(())
+            }
+            None => Err(format!("Could not find order by id {order_id}")),
+        }
+    }
 }
 
-struct OrderBook {
+struct OrderBook<'a> {
     // an ask is a buy order for Bitcoin
-    asks: Vec<Limit>,
+    asks: Vec<Limit<'a>>,
     // a bid is a sell order for Bitcoin
-    bids: Vec<Limit>,
+    bids: Vec<Limit<'a>>,
 }
 
 #[cfg(test)]
@@ -67,9 +91,28 @@ pub mod tests {
         let buy_order = Order::new_bid(5.0);
 
         // When
-        limit.add_order(buy_order);
+        limit.add_order(&buy_order);
 
         // Then
         assert_eq!(limit.orders.len(), 1);
+    }
+
+    #[test]
+    fn successfully_removes_a_buy_order_from_a_limit() {
+        // Given
+        let mut limit = Limit::new(10_000.0);
+        let buy_order_a = Order::new_bid(5.0);
+        let buy_order_b = Order::new_bid(8.0);
+        let buy_order_c = Order::new_bid(10.0);
+
+        limit.add_order(&buy_order_a);
+        limit.add_order(&buy_order_b);
+        limit.add_order(&buy_order_c);
+
+        // When
+        let result = limit.remove_order(buy_order_b.id());
+
+        // Then
+        assert_eq!(limit.total_volume, 15.0);
     }
 }
